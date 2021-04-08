70d671b686169cd031aa18fcf2764898853be939
Alexander Ivankov <ffmoneyff@gmail.com>
2021-04-08 23:32:02 +0300

    chancgecode

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int miles) {
-        int price = 17000;
-        miles = price / 20;
+    public int calculate(int price) {
+        int miles = price / 20;
         return miles;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 17_000;
-        int miles = service.calculate(price);
-        System.out.println(miles);
+        int miles1 = service.calculate(16_500);
+        System.out.println(miles1);
+
+        int miles2 = service.calculate(20_000);
+        System.out.println(miles2);
+
+        int miles3 = service.calculate(10_200);
+        System.out.println(miles3);
+
+        int miles4 = service.calculate(30_150);
+        System.out.println(miles4);
+
     }
 }
